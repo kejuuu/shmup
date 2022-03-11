16b0ff4a1e619ba0c366c8c2d24be9bc1e58c19f
Keju <kelvinjulian.kj@gmail.com>
2022-03-11 16:38:55 +0700

    collision checker

---
+++ src/utils/Collision.java
@@ -0,0 +1,10 @@
+package utils;
+import java.awt.Rectangle;
+
+public class Collision {
+    public static boolean checkCollision(GameObject a, GameObject b) {
+        Rectangle ra = new Rectangle((int) a.transform.positionX, (int) a.transform.positionY, a.image.getWidth(), a.image.getHeight());
+        Rectangle rb = new Rectangle((int) b.transform.positionX, (int) b.transform.positionY, b.image.getWidth(), b.image.getHeight());
+        return ra.intersects(rb);
+    }    
+}
